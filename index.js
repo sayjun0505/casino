fb796df49192ddab3c27144347f1962f7228bbb2
sayjun0505 <sayjun0505@gmail.com>
2024-01-12 17:26:00 +0100

    w

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 // Add Express
 const express = require("express");
-
 // Initialize Express
 const app = express();
+const axios = require('axios');
 
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 7071 });
 // Create GET request
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
-
+app.get("/test", (req, res) => {
+    res.send({status: 'success'});
+})
 // Initialize server
 app.listen(5000, () => {
   console.log("Running on port 5000.");
