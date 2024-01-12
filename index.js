f5f4fde95b1eaaef43af373022d1675f6123d663
sayjun0505 <sayjun0505@gmail.com>
2024-01-12 17:32:12 +0100

    socket

--- index.js
+++ index.js
@@ -4,8 +4,38 @@ const express = require("express");
 const app = express();
 const axios = require('axios');
 
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 7071 });
+const { Server } = require('socket.io');
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+      origin: '*',
+    },
+  });
+
+
+  io.on('connection', (socket) => {
+    socket.on('sendnotification', (notification) => {
+      io.emit('sendnotification', notification);
+      pushnotification(notification);
+      unseennotification(notification);
+    });
+    socket.on('offer', (offerId) => {
+      io.emit('offer', offerId);
+    });
+    socket.on('received', (email) => {
+      io.emit('received', email);
+    });
+    socket.on('new quote', (email) => {
+      io.emit('new quote', email);
+    });
+    socket.on('disconnect', () => {
+      console.log('Socket disconnected:', socket.id);
+    });
+  });
+  
+
+
+
 // Create GET request
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
@@ -17,4 +47,5 @@ app.get("/test", (req, res) => {
 app.listen(5000, () => {
   console.log("Running on port 5000.");
 });
+
 module.exports = app;
