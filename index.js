830cd3498c942bdd23a2ce340bf6201df80e2ad9
sayjun0505 <sayjun0505@gmail.com>
2024-01-12 17:24:14 +0100

    ws

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-const app = require('express')();
-const { v4 } = require('uuid');
-// app.use(app.static('public'))
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+// Add Express
+const express = require("express");
+
+// Initialize Express
+const app = express();
+
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
 });
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+// Initialize server
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
 });
-app.get("/test", (req, res) => {
-    res.send({status: 'success'});
-})
-app.listen(4000, () => {
-    console.log('running on port 4000');
-    // start();
-})
 module.exports = app;
